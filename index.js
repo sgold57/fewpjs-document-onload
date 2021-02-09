af76ec03c702eaa8cc0681a8325d3548f352b0e4
Sam Gold <samg915757@gmail.com>
2021-02-09 14:10:22 -0700

    commit tree fdb4420e95ab217454a8eeea0fb00631285d1bac

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const pToChange = document.getElementById('text')
+  pToChange.textContent = "This is really cool!"
+})
